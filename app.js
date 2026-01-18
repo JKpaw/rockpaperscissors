ce0da5d807d015fca4be0b735a4e0e55effdabb2
JKpaw <81526591+JKpaw@users.noreply.github.com>
2026-01-18 20:34:28 +0200

    implement computer and human choice functions for rock-paper-scissors game

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("Hello World");
+
+getComputerChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
+console.log(getComputerChoice())
+
+getHumanChoice = () => {
+    prompt("Rock, paper or scissors?", "");
+    let choice = ""
+    if (choice === "rock") {
+      return "rock";
+    } else if (choice === "paper") {
+      return "paper";
+    } else if (choice === "scissors") {
+      return "scissors"; }
+      else {
+        console.log("Invalid Guess")
+      }
+    }
+
+console.log(getHumanChoice())
+// Returns either 'rock', 'paper', or 'scissors'. 
+
